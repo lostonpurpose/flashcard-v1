429ff059d28185b2508134fd8a5c65e8f83fbc75
Will VanderWyden <vanderwyden@gmail.com>
2025-12-05 18:53:21 +0900

    basic functions working

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-import kanjiData from './kanji.json' assert { type: 'json' };
+const fs = require('fs');
+
+// 1. Read file as text
+const rawData = fs.readFileSync('kanji.json', 'utf8');
+
+// 2. Parse JSON
+const flashcards = JSON.parse(rawData);
+
+console.log(flashcards);
